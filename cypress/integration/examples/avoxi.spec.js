49951bb0e234dd5bdfec65dbb4ccf4738a58b251
yohannes <yohannesbog@gmail.com>
2019-05-14 21:35:05 -0400

    add test

--- cypress/integration/examples/avoxi.spec.js
+++ cypress/integration/examples/avoxi.spec.js
@@ -8,7 +8,19 @@ describe('avoxi cart', () => {
  
         // cy.wait(3000)
         cy.get('[id="colophon"]').should('be.visible')
-        cy.viewport(500, 500)
+        // cy.viewport(500, 500)
+        cy.wait(3000)
         cy.get('#menu-item-1397 > a')   
+        cy.get('.block-bg-overlay').click()
+        cy.get('#menu-item-4866 > a').click()
+        cy.get('.header > .container > .row > .col-md-8 > .header__hero-btns > .button-orange').click()
+    })
+
+    it('got to link and provide personal info', () => {
+         cy.visit('https://app.testproject.io/signup/')
+        cy.get('[id="email"]').type('yohannes@gmail.com')
+        cy.get('[id="company"]').type('Avoxi')
+        cy.get('[id="first-name"]').type('Avoxi')
+        cy.get('[id="last-name"]').type('Avoxi')
      })
 })
