8a450234d405f878e1f1e39e06803d5116ce7147
yohannes <yohannesbog@gmail.com>
2019-05-16 21:53:48 -0400

    add test

--- cypress/integration/examples/avoxi.spec.js
+++ cypress/integration/examples/avoxi.spec.js
@@ -7,24 +7,30 @@ describe('avoxi cart', () => {
     })
 
     it('scroll to the bootom of the page and select ', () => {
- 
         // cy.wait(3000)
         cy.get('[id="colophon"]').should('be.visible')
         // cy.viewport(500, 500)
         // cy.wait(3000)
-
         cy.get('#menu-item-1397 > a')
         cy.get('.block-bg-overlay').click()
         cy.get('#menu-item-4866 > a').click()
-
-        // cy.get('.header > .container > .row > .col-md-8 > .header__hero-btns > .button-orange').click()
     })
 
     it('got to link and provide personal info', () => {
-         cy.visit('https://app.testproject.io/signup/')
+        cy.visit('https://app.testproject.io/signup/')
         cy.get('[id="email"]').type('yohannes@gmail.com')
         cy.get('[id="company"]').type('Avoxi')
         cy.get('[id="first-name"]').type('Avoxi')
         cy.get('[id="last-name"]').type('Avoxi')
+     })
+     //inter password
+     it('I enter my password', () => {
+        cy.get('#password').type('Ab123456')
+        cy.get('#confirm-password').type('Ab123456')
+        cy.get('.tp-checkbox-input-view')
+     })
+     
+     it('Should click on agreement chheckbox', () => {
+        cy.get('.tp-checkbox-input-view').click()
      })
 })
