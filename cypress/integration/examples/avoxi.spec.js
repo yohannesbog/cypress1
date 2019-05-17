dc46831caa6478a5e5ad88605af0712a7e65d258
yohannes <yohannesbog@gmail.com>
2019-05-16 21:25:02 -0400

    coment test

--- cypress/integration/examples/avoxi.spec.js
+++ cypress/integration/examples/avoxi.spec.js
@@ -1,19 +1,20 @@
 describe('avoxi cart', () => {
 
-    it('Navigate to avoxi shoping site', () => {
+    it('testproject site', () => {
         cy.visit('https://blog.testproject.io/2017/06/07/javascript-testing-with-selenium-webdriver-mocha/')
     })
 
-    it('select country', () => {
+    it('scroll to the bootom of the page and select ', () => {
  
         // cy.wait(3000)
         cy.get('[id="colophon"]').should('be.visible')
         // cy.viewport(500, 500)
         cy.wait(3000)
-        cy.get('#menu-item-1397 > a')   
+
+        cy.get('#menu-item-1397 > a')
         cy.get('.block-bg-overlay').click()
         cy.get('#menu-item-4866 > a').click()
-        cy.get('.header > .container > .row > .col-md-8 > .header__hero-btns > .button-orange').click()
+        // cy.get('.header > .container > .row > .col-md-8 > .header__hero-btns > .button-orange').click()
     })
 
     it('got to link and provide personal info', () => {
